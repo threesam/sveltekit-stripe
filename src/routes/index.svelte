1229cba60d7652a1ad848803891a274f73f4f3ea
threesam <salvatoredangelo@protonmail.com>
2021-12-23 22:41:14 -0500

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,48 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+
+	export async function load({ fetch }) {
+		const res = await fetch('/plans.json');
+		const plans = await res.json();
+		return {
+			status: 200,
+			props: {
+				plans
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Pricing from '$lib/Pricing/index.svelte';
+	import StripeProvider from '$lib/stripe/StripeProvider.svelte';
+
+	export let plans;
+</script>
+
+<svelte:head>
+	<title>Pricing</title>
+</svelte:head>
+
+<StripeProvider>
+	<section>
+		<h2>Choose your plan and start counting!!!</h2>
+		<Pricing {plans} />
+	</section>
+</StripeProvider>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h2 {
+		color: var(--heading-color);
+		font-weight: bold;
+		font-size: 2rem;
+	}
+</style>
